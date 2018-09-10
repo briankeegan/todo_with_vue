11a8349ea0bebfc3f2cff015a191d5da382dc978
Brian <brian.morris.keegan@gmail.com>
2018-09-09 21:42:56 -0400

    Add ability to checkoff todos

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const app = new Vue({
     headerTitle: 'Todo App with Vue',
     inputPlaceholder: 'Write your todos',
     inputValue: '',
-    todos: ['asdf']
+    todos: []
   },
   methods: {
     createTodo: function () {
-      this.todos.push(this.inputValue);
+      this.todos.push({ text: this.inputValue, done: false });
       this.inputValue = '';
     },
     deleteTodo: function (index) {
       this.todos.splice(index, 1)
+    },
+    toggleDone: function (index) {
+      this.todos[index].done = !this.todos[index].done;
     }
   }
 });
